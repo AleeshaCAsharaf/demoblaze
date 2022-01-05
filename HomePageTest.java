243df6856f9aaf3079afe3b28d86b1cf3663902d
AleeshaCAsharaf <91299776+AleeshaCAsharaf@users.noreply.github.com>
2022-01-05 09:32:44 +0530

    Add files via upload

---
+++ HomePageTest.java
@@ -0,0 +1,25 @@
+package com.POM.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.POM.base.BaseClass;
+
+
+import com.POM.pages.ProductPage;
+
+public class HomePageTest extends BaseClass {
+	
+	
+	ProductPage productPage;
+	
+	@Test
+	public void homePageTest() {
+		productPage = homePage.products();
+		String expectedURL ="https://www.demoblaze.com/#";
+		String actualURL = driver.getCurrentUrl();
+		Assert.assertEquals(actualURL, expectedURL);
+		
+	}
+
+}
