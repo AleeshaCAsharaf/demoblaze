ba80a1641281dcc74fe0a8ac7a0001c5009b1b7f
AleeshaCAsharaf <91299776+AleeshaCAsharaf@users.noreply.github.com>
2022-01-05 09:32:03 +0530

    Add files via upload

---
+++ ProductDisplayPage.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.POM.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.POM.base.BaseClass;
+
+/**
+ * @author Admin
+ *
+ */
+public class ProductDisplayPage extends BaseClass{
+	@FindBy(xpath="//a[@onClick='addToCart(8)']")
+	WebElement ProductCart;
+	
+	public ProductDisplayPage() {
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public void AddToCart() {
+		ProductCart.click();
+	}
+
+}
---
+++ ProductPage.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.POM.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.POM.base.BaseClass;
+
+/**
+ * @author Admin
+ *
+ */
+public class ProductPage extends BaseClass {
+	@FindBy(xpath="//a[@href='prod.html?idp_=8']//img[@class='card-img-top img-fluid']")
+	WebElement product;
+	
+	public ProductPage() {
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public ProductDisplayPage clickOnProduct() {
+		product.click();
+		return new ProductDisplayPage();
+	}
+}
